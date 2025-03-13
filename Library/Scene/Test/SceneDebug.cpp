d5e403b49dca6d5821ed4179c5f80c8818645887
大和 中尾 <ny0107ny@outlook.jp>
2025-03-13 23:30:01 +0900

    ファイルパス

--- Library/Scene/Test/SceneDebug.cpp
+++ Library/Scene/Test/SceneDebug.cpp
@@ -37,7 +37,7 @@ void SceneDebug::Initialize()
     );
     ID3D11Device* device = Graphics::Instance().GetDevice();
 
-    SetSkyMap(L"./Data/SkyMap/S0.dds", 
+    SetSkyMap(L"./Data/SkyMap/kloofendal_48d_partly_cloudy_puresky_4k/sheen_pmrem.dds", 
         L"./Data/SkyMap/kloofendal_48d_partly_cloudy_puresky_4k/diffuse_iem.dds",
         L"./Data/SkyMap/kloofendal_48d_partly_cloudy_puresky_4k/specular_pmrem.dds");
 

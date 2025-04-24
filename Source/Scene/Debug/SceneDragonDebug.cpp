d9953c6d416179daaf5404b17312e12068cd4801
NakaoYamato <yamato.ousan@gmail.com>
2025-04-24 19:45:57 +0900

    シーン追加

---
+++ Source/Scene/Debug/SceneDragonDebug.cpp
@@ -0,0 +1,43 @@
+#include "SceneDragonDebug.h"
+
+#include "../../Library/Graphics/Graphics.h"
+
+// コンポーネント
+#include "../../Library/Component/Light/PointLightController.h"
+#include "../../Library/Component/ShapeController.h"
+
+#include "../../Library/Component/ModelRenderer.h"
+#include "../../Source/Actor/Dragon/DragonActor.h"
+
+void SceneDragonDebug::Initialize()
+{
+    Scene::Initialize();
+
+    ID3D11Device* device = Graphics::Instance().GetDevice();
+
+    SetSkyMap(L"./Data/SkyMap/kloofendal_48d_partly_cloudy_puresky_4k/sheen_pmrem.dds",
+        L"./Data/SkyMap/kloofendal_48d_partly_cloudy_puresky_4k/diffuse_iem.dds",
+        L"./Data/SkyMap/kloofendal_48d_partly_cloudy_puresky_4k/specular_pmrem.dds");
+
+    // オブジェクト作成
+    ActorManager& actorManager = GetActorManager();
+    {
+        auto stage = RegisterActor<Actor>("Stage", ActorTag::Stage);
+
+        //stage->GetTransform().SetPositionY(-2.7f);
+        //stage->GetTransform().SetLengthScale(0.1f);
+        //auto modelCont = stage->AddComponent<ModelRenderer>("./Data/Model/Stage/Land/Land.fbx");
+
+        stage->GetTransform().SetLengthScale(0.01f);
+        auto modelCont = stage->AddComponent<ModelRenderer>("./Data/Model/Stage/Test/test.fbx");
+
+        modelCont->SetRenderType(ModelRenderType::Static);
+    }
+    {
+        auto box = RegisterActor<Actor>("box", ActorTag::Player);
+        auto boxC = box->AddComponent<ShapeController>();
+    }
+    {
+        auto dragon = RegisterActor<DragonActor>("Dragon", ActorTag::Enemy);
+    }
+}

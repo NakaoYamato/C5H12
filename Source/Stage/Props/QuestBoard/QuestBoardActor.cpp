2236d267bb2bf3eccf4be8aa29b243949ebfd07c
NakaoYamato <yamato.ousan@gmail.com>
2026-01-09 16:22:10 +0900

    クエストボードモデル

--- Source/Stage/Props/QuestBoard/QuestBoardActor.cpp
+++ Source/Stage/Props/QuestBoard/QuestBoardActor.cpp
@@ -2,7 +2,6 @@
 
 #include "../../Library/Scene/Scene.h"
 #include "../../Library/Component/ModelRenderer.h"
-#include "../../Library/Component/Collider/SphereCollider.h"
 #include "../../Library/Component/Collider/BoxCollider.h"
 #include "../../Source/Common/InteractionController.h"
 
@@ -12,7 +11,7 @@
 void QuestBoardActor::OnCreate()
 {
 	// モデルロード
-	//LoadModel("./Data/Model/Stage/Props/Chest/ChestBottom.fbx");
+	LoadModel("./Data/Model/Stage/Props/QuestBoard/QuestBoard.fbx");
 
 	// コンポーネント追加
 	auto modelRenderer = this->AddComponent<ModelRenderer>();
@@ -21,17 +20,19 @@ void QuestBoardActor::OnCreate()
 
 	// コライダー追加
 	auto bodyCollider = this->AddCollider<BoxCollider>();
-	auto judgeCollider = this->AddCollider<SphereCollider>();
+	auto judgeCollider = this->AddCollider<BoxCollider>();
 
 	// パラメータ設定
+	GetTransform().SetLengthScale(0.01f);
 	modelRenderer->SetRenderType(ModelRenderType::Static);
 	bodyCollider->SetLayer(CollisionLayer::Stage);
 	bodyCollider->SetLayerMask(GetCollisionLayerMaskExcept(CollisionLayer::Stage));
 	bodyCollider->SetTrigger(false);
-	bodyCollider->SetPosition(Vector3(0.0f, 1.0f, 0.0f));
-	bodyCollider->SetRadius(Vector3(1.0f, 1.0f, 0.3f));
+	bodyCollider->SetPosition(Vector3(0.0f, 170.0f, 0.0f));
+	bodyCollider->SetRadius(Vector3(1.8f, 1.5f, 0.2f));
 	judgeCollider->SetLayer(CollisionLayer::Stage);
 	judgeCollider->SetLayerMask(GetCollisionLayerMaskExcept(CollisionLayer::Stage));
 	judgeCollider->SetTrigger(true);
-	judgeCollider->SetPosition(Vector3(0.0f, 1.0f, 0.0f));
+	judgeCollider->SetPosition(Vector3(0.0f, 170.0f, -100.0f));
+	judgeCollider->SetRadius(Vector3(1.8f, 1.5f, 0.5f));
 }

82bb8315222fc716111952ebe69eea1a9342b8d9
NakaoYamato <yamato.ousan@gmail.com>
2026-01-16 16:29:35 +0900

    ステートマシン改造例

--- Library/Component/StateController.cpp
+++ Library/Component/StateController.cpp
@@ -26,3 +26,45 @@ void StateController::ChangeState(const char* mainStateName, const char* subStat
 {
 	_stateMachine->ChangeState(mainStateName, subStateName);
 }
+
+class TestSMC;
+
+class TestHSB : public HierarchicalStateBase<TestSMC>
+{
+public:
+	TestHSB(TestSMC* stateMachine) : HierarchicalStateBase<TestSMC>(stateMachine) {}
+	~TestHSB() override {}
+	const char* GetName() const { return "TestHSB"; }
+	// 開始処理
+	virtual void OnEnter() override
+	{
+	}
+	// 更新処理
+	virtual void OnExecute(float elapsedTime) override
+	{
+	}
+	// 終了処理
+	virtual void OnExit() override
+	{
+	}
+};
+
+class TestSMC : public StateController2<TestSMC>
+{
+public:
+	TestSMC(std::shared_ptr<StateController2::SM> stateMachine) :
+		StateController2(stateMachine)
+	{
+	}
+
+	void OnStart() override
+	{
+		_stateMachine->RegisterState(std::make_shared<TestHSB>(this));
+	}
+	void OnLateUpdate(float elapsedTime) override
+	{
+
+	}
+};
+
+static TestSMC testSMCInstance(std::make_shared<StateMachineBase<TestSMC>>());
--- Library/Component/StateController.h
+++ Library/Component/StateController.h
@@ -59,3 +59,97 @@ public:
 private:
 	std::shared_ptr<StateMachine> _stateMachine = nullptr;
 };
+
+template<typename T>
+class StateController2 : public Component
+{
+public:
+	using SM = StateMachineBase<T>;
+
+public:
+	StateController2(std::shared_ptr<SM> stateMachine) :_stateMachine(stateMachine) {}
+	~StateController2() override {}
+	// 名前取得
+	const char* GetName() const override { return "StateController2"; }
+	// 開始処理
+	void Start() final override
+	{
+		OnStart();
+		// セットアップ済みフラグを立てる
+		SetSetup(true);
+	}
+	// 遅延更新処理
+	void LateUpdate(float elapsedTime) final override
+	{
+		OnLateUpdate(elapsedTime);
+		_stateMachine->Update(elapsedTime);
+	}
+	// GUI描画
+	void DrawGui() final override
+	{
+		OnDrawGui();
+	}
+
+	// ステート変更
+	virtual void ChangeState(const char* mainStateName, const char* subStateName)
+	{
+		// ステートの変更を受け付けない場合は何もしない
+		if (!CanChangeState())
+			return;
+
+		// 遷移先が無効な場合は何もしない
+		if (mainStateName == nullptr || mainStateName[0] == '\0')
+			return;
+
+		std::string currentMainState = GetStateName();
+		std::string currentSubState = GetSubStateName();
+
+		// 現在のステートと変更先が違うなら変更
+		if (currentMainState != mainStateName)
+			_stateMachine->ChangeState(mainStateName);
+		// サブステートがあるなら変更
+		if (subStateName != nullptr && subStateName[0] != '\0')
+		{
+			// 現在のサブステートと変更先が違うなら変更
+			if (currentSubState != subStateName)
+				_stateMachine->ChangeSubState(subStateName);
+		}
+	}
+	// ステート名取得
+	virtual const char* GetStateName()
+	{
+		if (!_stateMachine->GetState())
+			return "";
+		return _stateMachine->GetState()->GetName();
+	}
+	// サブステート名取得
+	virtual const char* GetSubStateName()
+	{
+		if (!_stateMachine->GetState())
+			return"";
+		return _stateMachine->GetState()->GetSubStateName();
+	}
+	// ステートマシン取得
+	std::shared_ptr<SM> GetStateMachine() { return _stateMachine; }
+
+	// セットアップ済みかどうか
+	bool IsSetup() const { return _isSetup; }
+	// セットアップ済みフラグを設定
+	void SetSetup(bool isSetup) { _isSetup = isSetup; }
+	// ステートの変更を受け付けるか取得
+	bool CanChangeState() const { return _canChangeState; }
+	// ステートの変更を受け付けるか設定
+	void SetCanChangeState(bool canChangeState) { _canChangeState = canChangeState; }
+protected:
+	virtual void OnStart() {}
+	virtual void OnLateUpdate(float elapsedTime) {}
+	virtual void OnDrawGui() {}
+
+protected:
+	std::shared_ptr<SM> _stateMachine = nullptr;
+
+	// セットアップ済みフラグ
+	bool _isSetup = false;
+	// ステートの変更を受け付けるか
+	bool _canChangeState = true;
+};
